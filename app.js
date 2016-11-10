2eb89be63bac078115ef633551c7476ce49a73bb
Anthony Ascencio <anthony2025@gmail.com>
2016-11-09 22:09:02 -0500

    app running, routing not working

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-'use strict'
-// I can pass a string as a parameter, and it will check to see whether that string contains either
-// a unix timestamp or a natural language date (example: January 1, 20)
-// If it does, it returns both the Unix timestamp and the natural language form of that date.
-// If it does not contain a date or Unix timestamp, it returns null for those properties.
-
-// setting up express
 const express = require('express')
 const app = express()
-
-// setting up environment variables
 const PORT = process.env.PORT || 4000
 
-// setting up the express router
-const router = require('./router.js')
-app.use('/*', router)
+app.get('/:date'), (req, res) => {
+    const date = req.params.date
+    console.log(date)
+}
 
-// setting up a homepage
-app.get('/', express.static('index.html'))
+app.get('/', express.static('public'))
 
-// setting up the express server
 app.listen(PORT, () => console.log('App running on port: ' + PORT))
+
+const isValidTimestamp = (date) => {
+  return Number.isInteger(date) && date >= 0;
+}
